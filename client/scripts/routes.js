1ff91437aebefb905f2e3d5bbfcb5939abc91d54
Amitava saha <amitava@azuqua.com>
2016-02-07 17:32:07 +0530

    routes

--- client/scripts/routes.js
+++ client/scripts/routes.js
@@ -2,34 +2,33 @@
  * Created by amitava on 30/01/16.
  */
 import React from 'react';
-import {Route} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
+
+import {
+    HomeContainer
+} from './routes/home'
+
+import {
+    InstituteContainer
+} from './routes/institute';
 
 import {
     SearchContainer
 } from './routes/search';
 
-import {
-    InstituteContainer
-} from './routes/institute'
+import NotFound from './routes/misc/404';
+import ServerError from './routes/misc/501';
 
 import App from './app';
 
 export default (
-    <Route component={App}>
-        <Route path="/">
+    <Route path="/" component={App}>
+        <IndexRoute component={HomeContainer}/>
+        <Route path="search" component={SearchContainer} />
+        <Route path="institute" component={InstituteContainer} />
+        <Route path="admin" />
+        <Route path="manage" />
 
-        </Route>
-        <Route path="/search" component={SearchContainer}>
-
-        </Route>
-        <Route path="/institute" conponent={InstituteContainer}>
-
-        </Route>
-        <Route path="/admin">
-
-        </Route>
-        <Route path="/manage">
-
-        </Route>
+        <Route path="*" component={NotFound} status="404" />
     </Route>
-)
+);
