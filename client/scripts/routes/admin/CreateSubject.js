9f069334e543137e887967e22b6d58fa5cbf623c
Amitava saha <amitava@azuqua.com>
2016-03-03 15:45:26 +0530

    google webmaster code

--- client/scripts/routes/admin/CreateSubject.js
+++ client/scripts/routes/admin/CreateSubject.js
@@ -93,7 +93,7 @@ export default class CreateSubject extends React.Component{
 
     render(){
         const {fields: {name, description}, error, handleSubmit, submitting,category_store, subject_store, course_store} = this.props;
-        
+
         const subjects = reduce(subject_store.ids, (memo, i) => {
             const c = subject_store.entities[i];
             if(c.course._id == this.state.course)
